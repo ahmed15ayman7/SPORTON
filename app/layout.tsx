97e6995b2a98e21a284c20170665ca573c6a2611
Ahmed <ahmed15ayman7ahmed2002@gmail.com>
2024-12-12 17:16:15 +0200

    create seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,17 @@ export default function RootLayout({
                 name="google-site-verification"
                 content="4SotCEA8ZnMeI_GpUauosjYwpag6nhVQ00yvIYEhRZs"
               />
-
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KF63VLNP');
+            `,
+                }}
+              ></script>
               <script
                 async
                 src="https://www.googletagmanager.com/gtag/js?id=G-2YD5P70M8D"
@@ -105,6 +115,14 @@ export default function RootLayout({
             `,
                 }}
               />
+              <noscript>
+                <iframe
+                  src="https://www.googletagmanager.com/ns.html?id=GTM-KF63VLNP"
+                  height="0"
+                  width="0"
+                  style={{ display: "none", visibility: "hidden" }}
+                ></iframe>
+              </noscript>
             </body>
           </html>
         </ReduxProvider>
